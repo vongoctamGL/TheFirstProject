c591488361f9975e6aa78b16d33106056fb82fc2
vongoctamGL <vongoctamgl1995@gmail.com>
2018-12-30 08:44:05 +0700

    Update files
    Signed-off-by: vongoctamGL <vongoctamgl1995@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -18,14 +18,14 @@ gulp.task('browser-sync', function () {
         server: "app/dist"
     })
     gulp.watch('app/src/styles/**/*.sass', gulp.series('sass'));
-    gulp.watch('app/src/*.pug', gulp.series('pug'));
+    gulp.watch('app/src/templates/**/*.pug', gulp.series('pug'));
     gulp.watch('app/src/scripts/**/*.js', gulp.series('javascript'))
     gulp.watch('app/src/img/*',gulp.series('copy-img'))
     gulp.watch('app/dist/*.html').on('change', browserSync.reload);
 })
 //Biên dịch file PUG sang HTML
 gulp.task('pug', function buildHTML() {
-    return gulp.src('app/src/**/*.pug')
+    return gulp.src('app/src/templates/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('app/dist'))
         .pipe(browserSync.stream())
@@ -37,7 +37,6 @@ gulp.task('sass', function () {
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(postcss([autoprefixer(), cssnano()]))
-
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/dist/css'))
         .pipe(browserSync.stream())
